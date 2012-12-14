bd1dda2a7214197f409649ce340ceb4a58628abe
ec@Ubuntu <xiacijian@163.com>
2012-12-14 12:40:46 +0800

    添加Question010.java的注解

--- src/test/Question010.java
+++ src/test/Question010.java
@@ -2,16 +2,23 @@ package test;
 
 import org.junit.Test;
 
-
+/**
+ * 有n个学校 参加某运动会 其中 男子项目有x 个，女子项目有 y
+ * 因为每个项目根据参与人数不同 ，每个项目取名次也不同
+ * 现在假设 奇数项目取前7名，偶数项目取前5名
+ * 前7名的 积分分别是 7, 5, 3, 2, 1
+ * 前5名的 积分分别是 5，3，2
+ * 请计算每个学校的 男女积分排名 和总积分排名
+ * 请自行设计 测试参数*/
 public class Question010 {
 	
 	@Test
 	public void Q10(){
-		int school[] = new int[7];//初始化分数
-		int manNum[] = new int[7];//初始化分数
-		int womNum[] = new int[7];//初始化分数
-		int manXM = 12;
-		int womXM = 34;
+		int school[] = new int[7];//初始化学校总分数
+		int manNum[] = new int[7];//初始化男子分数
+		int womNum[] = new int[7];//初始化女子分数
+		int manXM = 12;//初始化 男子项目数
+		int womXM = 34;//初始化 女子项目数
 		System.out.println("男子比赛：");
 		int manSum[] =	tongji(manNum,manXM);
 		System.out.println("女子比赛：");
@@ -32,7 +39,7 @@ public class Question010 {
 		int random;
 		for(int i=1;i<=manXM;i++){
 			if(i%2 != 0){
-				schoolNum[getRandom(length)] += 7;//(int)Math.random()*length  0,1,2,3
+				schoolNum[getRandom(length)] += 7;//模拟得分情况
 				schoolNum[getRandom(length)] +=5;
 				schoolNum[getRandom(length)] +=3;
 				schoolNum[getRandom(length)] +=2;
@@ -61,7 +68,6 @@ public class Question010 {
 	}
 	
 	public int getRandom(int i){
-		
 		return Integer.parseInt((Math.random()*i+"").split("\\.")[0]);
 	}
 	
